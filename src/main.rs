62eedd42e34d1a6517a172be117174fd716d267e
Chakornpat.T <chakornpat0206@outlook.com>
2026-01-24 18:12:13 +0700

    Populate deck with cards

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,18 @@ struct Deck {
 }
 
 fn main() {
-    let deck = Deck { cards: vec![] };
+    let suits = ["Hearts", "Spades", "Diamonds"];
+    let values = ["Ace", "Two", "Three"];
+    let mut cards = vec![];
 
-    println!("Heres your deck: {:?}", deck);
+    for suit in suits {
+        for value in values {
+            let card = format!("{} of  {}", value, suit);
+            cards.push(card);
+        }
+    }
+
+    let deck = Deck { cards };
+
+    println!("Heres your deck: {:#?}", deck);
 }
