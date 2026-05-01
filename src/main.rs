5bf2535c77c6a8d544cb847c0317adc120113bb8
Chakornpat.T <chakornpat0206@outlook.com>
2026-05-02 01:30:00 +0700

    IMPROVE MEDIA DESCRIPITION FORMAT

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,20 @@ fn print_dedia(media: Media) {
     println!("{:#?}", media);
 }
 
+impl Media {
+    fn description(&self) -> String {
+        if let Media::Book { title, author } = self {
+            format!("Book {} {}", title, author)
+        } else if let Media::Audiobook { title } = self {
+            format!("Audiobook {} ", title)
+        } else if let Media::Movie { title, director } = self {
+            format!("Movie {} {}", title, director)
+        } else {
+            format!("Media Description")
+        }
+    }
+}
+
 fn main() {
     let audio_book = Media::Audiobook {
         title: "An audiobook".to_string(),
@@ -18,12 +32,12 @@ fn main() {
         title: "Good Movie".to_string(),
         director: "Good Director".to_string(),
     };
-    let bad_movie = Media::Book {
-        title: "Bad Movie".to_string(),
+    let bad_book = Media::Book {
+        title: "Bad book".to_string(),
         author: "bad author".to_string(),
     };
 
-    print_dedia(audio_book);
-    print_dedia(good_movie);
-    print_dedia(bad_movie);
+    println!("{}", audio_book.description());
+    println!("{}", good_movie.description());
+    println!("{}", bad_book.description());
 }
