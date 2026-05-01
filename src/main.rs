d5bee551939c93f41263da31598248466feb0752
Chakornpat.T <chakornpat0206@outlook.com>
2026-05-02 00:39:18 +0700

    Declaring Enum Values

--- src/main.rs
+++ src/main.rs
@@ -13,5 +13,17 @@ fn main() {
     let audio_book = Media::Audiobook {
         title: "An audiobook".to_string(),
     };
+
+    let good_movie = Media::Movie {
+        title: "Good Movie".to_string(),
+        director: "Good Director".to_string(),
+    };
+    let bad_movie = Media::Book {
+        title: "Bad Movie".to_string(),
+        author: "bad author".to_string(),
+    };
+
     print_dedia(audio_book);
+    print_dedia(good_movie);
+    print_dedia(bad_movie);
 }
