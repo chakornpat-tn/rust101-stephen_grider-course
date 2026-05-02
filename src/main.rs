d8148ac8046eaac216b8788defb0a3b7ced78588
Developer <developer@example.com>
2026-05-02 15:41:11 +0700

    refact desc

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,10 @@ fn print_dedia(media: Media) {
 
 impl Media {
     fn description(&self) -> String {
-        if let Media::Book { title, author } = self {
-            format!("Book {} {}", title, author)
-        } else if let Media::Audiobook { title } = self {
-            format!("Audiobook {} ", title)
-        } else if let Media::Movie { title, director } = self {
-            format!("Movie {} {}", title, director)
-        } else {
-            format!("Media Description")
+        match self {
+            Media::Book { title, author } => format!("Book {} {}", title, author),
+            Media::Audiobook { title } => format!("Book {}", title),
+            Media::Movie { title, director } => format!("Book {} {}", title, director),
         }
     }
 }
